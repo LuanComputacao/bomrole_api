8f8b196c647528caa9ae779ea9dcc477412bbb90
LuanComputacao <eng.luanroger@gmail.com>
2020-02-05 00:39:31 -0300

    5e3992c267e4a563e23a55a8 Search for tourist spots by name

---
+++ touristspots/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from touristspots.models import TouristSpot, Category
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
+
+
+class TouristSpotSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TouristSpot
+        fields = [
+            'latitude',
+            'longitude',
+            'name',
+            'category',
+        ]
+
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name']
