df3517fd65cbf4ee5372cc6cd1e3254c1a38762d
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2020-02-08 20:06:30 -0300

    users can upvote tourist spot

--- touristspots/serializers.py
+++ touristspots/serializers.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-from touristspots.models import TouristSpot, Category
+from touristspots.models import TouristSpot, Category, TouristSpotUpvote
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = [
+            'url',
+            'username',
+            'email',
+            'tourist_spots'
+        ]
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
@@ -20,15 +25,31 @@ class TouristSpotSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TouristSpot
         fields = [
-            'url',
-            'latitude',
-            'longitude',
             'name',
             'category',
+            'upvotes_count',
+            'latitude',
+            'longitude',
+            'registered_by',
+            'url'
         ]
+
+    upvotes_count = serializers.SerializerMethodField()
+
+    def get_upvotes_count(self, obj):
+        return obj.upvotes.count()
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Category
         fields = ['url', 'name']
+
+
+class TouristSpotUpvoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TouristSpotUpvote
+        fields = [
+            'user',
+            'tourist_spot'
+        ]
