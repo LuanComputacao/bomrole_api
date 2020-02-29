7d2ff121dd3d0d4be64774f60c54f319a07e575a
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2020-02-29 18:31:33 -0300

    docker running with PORT variable

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default
-cp .docker/default /etc/nginx/sites-available/default
+envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/sites-available/default
+# cp .docker/default /etc/nginx/sites-available/default
 service nginx start \
  && gunicorn bomrole.wsgi:application --bind 127.0.0.1:8000
