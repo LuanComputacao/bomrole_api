8316302ac340a1f77c45fbfb7bff7a85cdec3f1f
LuanComputacao <eng.luanroger@gmail.com>
2020-02-29 15:53:49 -0300

    nginx conf

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 rm /etc/nginx/sites-available/default
 cp .docker/default /etc/nginx/sites-available/default
-systemctl enable --now gunicorn.socket
 service nginx start \
- && gunicorn bomrole.wsgi:application --bind 0.0.0.0:8000
+ && gunicorn bomrole.wsgi:application --bind 127.0.0.1:8000
