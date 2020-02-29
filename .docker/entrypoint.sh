de9d5f121e8347ffac4077d7504246c7eb66061a
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2020-02-29 14:29:01 -0300

    serving docker gunicorn nginx django

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-service nginx start && gunicorn bomrole.wsgi:application --bind 0.0.0.0:$PORT
+rm /etc/nginx/sites-available/default
+cp .docker/default /etc/nginx/sites-available/default
+systemctl enable --now gunicorn.socket
+service nginx start \
+ && gunicorn bomrole.wsgi:application --bind 0.0.0.0:8000
