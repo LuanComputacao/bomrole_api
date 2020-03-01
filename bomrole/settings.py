58467caf68a0494b63a041dc0582e07880d6022a
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2020-03-01 15:56:50 -0300

    database config

--- bomrole/settings.py
+++ bomrole/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
+import dj_database_url
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -115,12 +117,14 @@ WSGI_APPLICATION = 'bomrole.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #     'default': {
+    #         # 'ENGINE': 'django.db.backends.sqlite3',
+    #         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
 }
+
+DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
