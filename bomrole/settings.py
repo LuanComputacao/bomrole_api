5b77fcb59da9df5150059be4a87a4de9b08be0fe
LuanComputacao <eng.luanroger@gmail.com>
2020-03-02 18:43:17 -0300

    postgis running

--- bomrole/settings.py
+++ bomrole/settings.py
@@ -117,14 +117,19 @@ WSGI_APPLICATION = 'bomrole.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    #     'default': {
-    #         # 'ENGINE': 'django.db.backends.sqlite3',
-    #         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
+    'default': {
+        #         # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_config = dj_database_url.config(
+    conn_max_age=600,
+    ssl_require=True
+)
+
+DATABASES['default'].update(db_config)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
