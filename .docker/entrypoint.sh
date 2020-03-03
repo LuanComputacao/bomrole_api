27047455244ca9882ead548063e06f191bab197c
LuanComputacao <eng.luanroger@gmail.com>
2020-03-02 22:40:45 -0300

    gunicorn logs

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,5 +7,5 @@ rm /etc/nginx/sites-available/default
 envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/sites-available/default
 # cp .docker/default /etc/nginx/sites-available/default
 bash /app/heroku-exec.sh
-service nginx start \
- && gunicorn bomrole.wsgi:application --bind 127.0.0.1:8000
+service nginx start
+gunicorn bomrole.wsgi:application --access-logfile - --error-logfile - --log-level debug --bind 127.0.0.1:8000
