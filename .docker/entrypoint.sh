5b8eb97ec8802b52250c8b67d446d489dac7bf5d
Anonymous <6576768+LuanComputacao@users.noreply.github.com>
2020-03-02 21:18:54 -0300

    executing heroku-exec

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -6,5 +6,6 @@ python manage.py collectstatic --noinput
 rm /etc/nginx/sites-available/default
 envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/sites-available/default
 # cp .docker/default /etc/nginx/sites-available/default
+bash /app/heroku-exec.sh
 service nginx start \
  && gunicorn bomrole.wsgi:application --bind 127.0.0.1:8000
